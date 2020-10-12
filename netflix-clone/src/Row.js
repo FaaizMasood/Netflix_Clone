295ac459e78b40182f363ab0b599944c630bdc1b
Faaiz <fmasood@clarku.edu>
2020-10-11 21:34:29 -0400

    Started the Project
    Started

---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Row({ title }) {
+  return (
+    <div>
+      {/* Title */}
+      <h2>{title}</h2>
+
+      {/* Container - posters */}
+    </div>
+  );
+}
+
+export default Row;
