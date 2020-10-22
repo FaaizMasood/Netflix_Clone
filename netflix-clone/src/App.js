6e8335e6bab4eafbf4cd6c524cf36df599956650
Faaiz <fmasood@clarku.edu>
2020-10-21 22:38:08 -0400

    Integrated Youtube trailers

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -2,15 +2,26 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./Requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
-      <h1> Hi There</h1>
+      <Nav />
+      <Banner />
+
       <Row
         title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="TDocumentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
-function Row({ title, fetchUrl }) {
+const base_url = "http://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
   // state is like short term memory , the way to wrote variables in react
   const [movies, setMovies] = useState([]); // we give initaial value to empty array
+  const [trailerUrl, setTrailerUrl] = useState("");
   // now we need to fill it with stuff
   //  we need a snippet of code which runs based on specific condition/ variable
   // we are trying to achiveve that when the row loads i want to make a request to tmdb to return the information
@@ -14,22 +19,57 @@ function Row({ title, fetchUrl }) {
       // "https//api.themoviedb.org/3", and the `/discover/tv?api_key=${API_KEY}&with_networks=213`,
       //console.log(axios.get(fetchUrl));
       const request = await axios.get(fetchUrl);
-      //console.log(request.data.results);
+      console.log(request);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
+  //console.table(movies);
+  // from react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-  console.log(movies);
+  //gets only the specific video trailer to play when clicked on
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      //onsole.log(trailerUrl);
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //console.log(url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+          //
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  //
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
-          <img src={movie.poster_path} alt={movie.name} />
+          //
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
